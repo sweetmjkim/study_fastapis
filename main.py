42ebea8d7e746cbc5ce2bc35572451751ccbd2e5
sweetmjkim <sweet.mjkim@gmail.com>
2024-01-02 18:49:42 +0900

    main 수정

--- main.py
+++ main.py
@@ -12,30 +12,27 @@ app.add_middleware(
     allow_headers=["*"],
 )
 from routes.homes import router as event_router
+from routes.gadgets import router as gadgets
+from routes.positionings import router as positionings
 app.include_router(event_router, prefix='/home')
-
-from routes.gadgets import router as event_router
-app.include_router(event_router, prefix='/gadgets')
-
-from routes.positionings import router as event_router
-app.include_router(event_router, prefix='/positionings')
+app.include_router(gadgets, prefix='/gadgets')
+app.include_router(positionings, prefix='/positionings')
 
 from fastapi import Request
 from fastapi.templating import Jinja2Templates
-
 # html 틀이 있는 폴더 위치
 templates = Jinja2Templates(directory="templates/")
-
-@app.get("/")
-async def root(request:Request):
-    # return {"message": "gocolab World!"}
-    # html 틀로 호출
-    return templates.TemplateResponse("main.html"
-                                      ,{'request':request})
 
 @app.get("/index")
 async def index(request:Request):
     # return {"message": "gocolab World!"}
     # html 틀로 호출
     return templates.TemplateResponse("index.html"
+                                      ,{'request':request})
+    
+@app.get("/")
+async def root(request:Request):
+    # return {"message": "gocolab World!"}
+    # html 틀로 호출
+    return templates.TemplateResponse("main.html"
                                       ,{'request':request})
